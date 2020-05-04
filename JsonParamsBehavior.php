17eabaa73e1ce147f34c11309867569c1038089c
denis909 <denis909@mail.ru>
2020-05-04 21:13:27 +0300

    n/a

--- JsonParamsBehavior.php
+++ JsonParamsBehavior.php
@@ -9,5 +9,28 @@ class JsonParamsBehavior extends \yii\base\Behavior
 
     public $attribute = 'params_json';
 
+    public function getParams() : array
+    {
+        if ($this->owner->{$this->attribute})
+        {
+            return Json::decode($this->owner->{$this->attribute}, true);
+        }
+
+        return [];
+    }
+
+    public function setParams(array $params)
+    {
+        $this->owner->{$this->attribute} = Json::encode($params);
+    }
+
+    public function setParam($name, $value)
+    {
+        $params = $this->getParams();
+
+        $params[$name] = $value;
+
+        $this->setParams($params);
+    }
 
 }
